9a390486bf0a0bd7b02dc79b32e0e71e2f85a14f
benny0912 <bennykeanhoe.ooi@sjsu.edu>
2020-01-22 14:02:41 -0800

    Initial Stage Tip Calculator

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Benny Ooi Kean Hoe on 1/20/20.
+//  Copyright © 2020 Benny Ooi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentage = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
